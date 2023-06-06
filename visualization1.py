d7ff4bafec90ae3eda821a9729f6613d02f48e37
Bahauddin Habibullah <84557850+bahau88@users.noreply.github.com>
2023-06-06 14:03:02 +0200

    Update visualization1.py

--- visualization1.py
+++ visualization1.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import pandas as pd
+from plotly.subplots import make_subplots
 
 # Load the data
-merged_df = pd.read_csv('https://raw.githubusercontent.com/bahau88/G2Elab-Energy-Building-/main/dataset/combined_data_green-er_2020_2023.csv') 
+merged_df = pd.read_csv('your_data.csv')  # Replace 'your_data.csv' with your actual data file
+
 # Convert the date column to a datetime object
 merged_df['Date'] = pd.to_datetime(merged_df['Date'])
 
@@ -33,29 +34,55 @@ fig.update_layout(
 # Add hover information
 fig.update_traces(hovertemplate='%{y:.2f}')
 
+# Create the Streamlit app
+st.title('Data Visualization')
+
+# Checkbox to select the data to show
+selected_data = st.multiselect('Select data to show', ['Consumption', 'Other', 'Heating', 'Lighting', 'All'],
+                               default=['All'])
+
+# Filter the data based on the user's selection
+if 'All' in selected_data:
+    selected_data = ['Consumption', 'Other', 'Heating', 'Lighting']
+
+selected_traces = []
+for data in selected_data:
+    if data == 'Consumption':
+        selected_traces.append(fig['data'][0])
+    elif data == 'Other':
+        selected_traces.append(fig['data'][1])
+    elif data == 'Heating':
+        selected_traces.append(fig['data'][2])
+    elif data == 'Lighting':
+        selected_traces.append(fig['data'][3])
+
+# Update the figure with the selected traces
+fig.update(data=selected_traces)
+
+# Display the figure
+st.plotly_chart(fig)
 
 # Create the subplots
-fig_exogeneous = make_subplots(rows=2, cols=3, subplot_titles=('Number of Room', 'Dayindex', 'Occupants',
-                                                               'Temperature', 'Cloudcover', 'Visibility'))
+fig_exogeneous = make_subplots(rows=2, cols=3, subplot_titles=('Number of Room', 'Dayindex', 'Occupants', 'Temperature', 'Cloudcover', 'Visibility'))
 
 # Add the traces to the subplots
 fig_exogeneous.add_trace(go.Scatter(x=merged_df['Date'], y=merged_df['Number of Room'], name='Number of Room',
-                                   line=dict(color='Coral', width=2), fill='tozeroy'), row=1, col=1)
+                         line=dict(color='Coral', width=2), fill='tozeroy'), row=1, col=1)
 
 fig_exogeneous.add_trace(go.Scatter(x=merged_df['Date'], y=merged_df['Dayindex'], name='Dayindex',
-                                   line=dict(color='orange', width=2), fill='tozeroy'), row=1, col=2)
+                         line=dict(color='orange', width=2), fill='tozeroy'), row=1, col=2)
 
 fig_exogeneous.add_trace(go.Scatter(x=merged_df['Date'], y=merged_df['Occupants'], name='Occupants',
-                                   line=dict(color='Crimson', width=2), fill='tozeroy'), row=1, col=3)
+                         line=dict(color='Crimson', width=2), fill='tozeroy'), row=1, col=3)
 
 fig_exogeneous.add_trace(go.Scatter(x=merged_df['Date'], y=merged_df['Temperature'], name='Temperature',
-                                   line=dict(color='blue', width=2), fill='tozeroy'), row=2, col=1)
+                         line=dict(color='blue', width=2), fill='tozeroy'), row=2, col=1)
 
 fig_exogeneous.add_trace(go.Scatter(x=merged_df['Date'], y=merged_df['Cloudcover'], name='Cloudcover',
-                                   line=dict(color='DarkCyan', width=2), fill='tozeroy'), row=2, col=2)
+                         line=dict(color='DarkCyan', width=2), fill='tozeroy'), row=2, col=2)
 
 fig_exogeneous.add_trace(go.Scatter(x=merged_df['Date'], y=merged_df['Visibility'], name='Visibility',
-                                   line=dict(color='purple', width=2), fill='tozeroy'), row=2, col=3)
+                         line=dict(color='purple', width=2), fill='tozeroy'), row=2, col=3)
 
 # Set the axis titles
 fig_exogeneous.update_xaxes(title_text='Date', row=1, col=1)
@@ -72,47 +99,5 @@ fig_exogeneous.update_traces(hovertemplate='%{y:.2f}')
 # Update the layout
 fig_exogeneous.update_layout(plot_bgcolor='white', showlegend=False)
 
-# Create the Streamlit app
-st.title('Data Visualization')
-
-# Checkbox to select the data to show
-selected_data = st.multiselect('Select data to show', ['Consumption', 'Other', 'Heating', 'Lighting', 'All',
-                                                       'Number of Room', 'Dayindex', 'Occupants',
-                                                       'Temperature', 'Cloudcover', 'Visibility'],
-                               default=['All'])
-
-# Filter the data based on the user's selection
-if 'All' in selected_data:
-    selected_data = ['Consumption', 'Other', 'Heating', 'Lighting', 'Number of Room', 'Dayindex', 'Occupants',
-                     'Temperature', 'Cloudcover', 'Visibility']
-
-selected_traces = []
-for data in selected_data:
-    if data == 'Consumption':
-        selected_traces.append(fig['data'][0])
-    elif data == 'Other':
-        selected_traces.append(fig['data'][1])
-    elif data == 'Heating':
-        selected_traces.append(fig['data'][2])
-    elif data == 'Lighting':
-        selected_traces.append(fig['data'][3])
-    elif data == 'Number of Room':
-        selected_traces.append(fig_exogeneous['data'][0])
-    elif data == 'Dayindex':
-        selected_traces.append(fig_exogeneous['data'][1])
-    elif data == 'Occupants':
-        selected_traces.append(fig_exogeneous['data'][2])
-    elif data == 'Temperature':
-        selected_traces.append(fig_exogeneous['data'][3])
-    elif data == 'Cloudcover':
-        selected_traces.append(fig_exogeneous['data'][4])
-    elif data == 'Visibility':
-        selected_traces.append(fig_exogeneous['data'][5])
-
-# Update the figure with the selected traces
-# Update the figure with the selected traces
-fig.update(data=selected_traces)
-fig_exogeneous.update(data=selected_traces)
-
-# Display the figure
+# Show the figure
 st.plotly_chart(fig_exogeneous)
