c9a0486fd937b83036985eb4d8255f45f8605b3a
Bahauddin Habibullah <84557850+bahau88@users.noreply.github.com>
2023-08-19 12:34:06 +0200

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -301,7 +301,7 @@ def predict_consumption2(num_hours, num_epochs, batch_size, variables):
     data.index.names = ['Datetime']
     
     # Split the data into input (X) and output (Y) variables
-    X = data[['Number of Room', 'Occupants', 'Temperature', 'Dayindex', 'Cloudcover', 'Visibility', ]].values
+    X = data[['Number of Room', 'Dayindex', 'Occupants', 'Temperature', 'Cloudcover', 'Visibility']].values
     #X = data[['Temperature', 'Dayindex' , 'Cloudcover', 'Visibility', 'Solarradiation']].values
     Y = data['Consumption'].values
     
@@ -356,7 +356,7 @@ def predict_consumption2(num_hours, num_epochs, batch_size, variables):
         cloudcover = cloudcover_arr[i]
         visibility = visibility_arr[i]
         #solarradiation = solarradiation_arr[i]
-        input_data[i] = [numberofroom, occupants, temperature, dayindex, cloudcover, visibility]
+        input_data[i] = [numberofroom, dayindex, occupants, temperature, cloudcover, visibility]
     
     input_data = (input_data - X_mean) / X_std
     input_data = input_data.reshape(input_data.shape[0], 1, input_data.shape[1])
